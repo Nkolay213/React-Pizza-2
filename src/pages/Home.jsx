211716fa078c1f357ad406900fa4ab242a41a012
Nkolay2711777 <sporofnet@gmail.com>
2024-02-27 18:30:54 +0300

    typesсript, после типизации стейта

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ import Sort, { sortList } from "../components/Sort";
 import PizzaBlock from "../components/PizzaBlock";
 import Skeleton from "../components/PizzaBlock/Skeleton";
 import Pagination from "../components/Pagination";
-import { SearchContext } from "../App";
+
 import { useDispatch, useSelector } from "react-redux";
 import {
   setCategoryId,
